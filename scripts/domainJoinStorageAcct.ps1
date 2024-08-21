c5548eb83626b796be3367451a5bce831bd50132
Jonathan.Core <jonathan@corefamily.us>
2024-08-21 19:04:49 -0400

    Refactor domainJoinStorageAcct.ps1 script to fix PowerShell 7 installation

--- scripts/domainJoinStorageAcct.ps1
+++ scripts/domainJoinStorageAcct.ps1
@@ -85,7 +85,7 @@ try {
 
     Write-Log -Message "VM is Domain Joined: $DomainJoined" -Type 'PRE-REQ'
 
-    write-Log -Message "Installing PowerShell 7" -Type 'Pre-REQ'
+    write-Log -Message "Installing PowerShell 7" -Type 'PRE-REQ'
     Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v7.4.4/PowerShell-7.4.4-win-x64.msi -OutFile C:\Windows\Temp\PowerShell-7.4.4-win-x64.msi -UseBasicParsing
     msiexec.exe /package C:\Windows\Temp\PowerShell-7.4.4-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
     Remove-Item -Path C:\Windows\Temp\PowerShell-7.4.4-win-x64.msi -Force
